8454920f3a8ba166da3492b4420ec014c52d0db8
MokrejBandita <ondrej.mudra@tesena.com>
2016-08-31 14:24:17 +0200

    changed endings of log file

--- script/executeTest.sh
+++ script/executeTest.sh
@@ -3,5 +3,5 @@
 mkdir jmeter
 wget -O jmeter/apache-jmeter-3.0.tgz http://mirror.dkm.cz/apache//jmeter/binaries/apache-jmeter-3.0.tgz -q
 tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
-jmeter/apache-jmeter-3.0/bin/jmeter -n -t test/travis.jmx -l jmeter.csv
-gzip jmeter.csv
+jmeter/apache-jmeter-3.0/bin/jmeter -n -t test/travis.jmx -l jmeter.jtl
+gzip jmeter.jtl
