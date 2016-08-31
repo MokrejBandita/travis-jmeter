bd0e0e85c50932351756c3ea30c1985c7e6e7fc2
MokrejBandita <ondrej.mudra@tesena.com>
2016-08-31 11:25:39 +0200

    added modified scripts taken from https://github.com/itaymendel/travis-jmeter

---
+++ script/executeTest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir jmeter
+wget -O jmeter/apache-jmeter-3.0.tgz http://mirror.dkm.cz/apache//jmeter/binaries/apache-jmeter-3.0.tgz -q
+tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
+jmeter/apache-jmeter-3.0/bin/jmeter -n -t test/travis.jmx -l -Jhost=localhost
+gzip results.jtl
