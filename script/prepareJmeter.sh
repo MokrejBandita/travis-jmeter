251b8ba9e77d3f3a9477a70292a2de0b022f15bd
MokrejBandita <ondrej.mudra@tesena.com>
2016-08-31 17:27:36 +0200

    added needed plugins

--- script/prepareJmeter.sh
+++ script/prepareJmeter.sh
@@ -4,7 +4,5 @@ mkdir jmeter
 wget -O jmeter/apache-jmeter-3.0.tgz http://mirror.dkm.cz/apache//jmeter/binaries/apache-jmeter-3.0.tgz -q
 tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
 
-wget -O jmeter/apache-jmeter-3.0/lib/ext/cmdrunner-2.0.jar http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
-
 cp config/user.properties jmeter/apache-jmeter-3.0/bin/
-cp -a config/plugins/. jmeter/apache-jmeter-3.0/lib/ext/
+cp -a config/plugins/. jmeter/apache-jmeter-3.0/lib/
