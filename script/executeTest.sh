af860b03d7210d1e4709f18a63e3888b84662e8f
MokrejBandita <ondrej.mudra@tesena.com>
2016-08-31 15:56:14 +0200

    Added new configuration files (user.properties for jmeter)

--- script/executeTest.sh
+++ script/executeTest.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mkdir jmeter
-wget -O jmeter/apache-jmeter-3.0.tgz http://mirror.dkm.cz/apache//jmeter/binaries/apache-jmeter-3.0.tgz -q
-tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
-jmeter/apache-jmeter-3.0/bin/jmeter -n -t test/travis.jmx -l jmeter.jtl
-gzip jmeter.jtl
+jmeter/apache-jmeter-3.0/bin/jmeter -n -t test/travis.jmx -l jmeter.csv
---
+++ script/prepareJmeter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir jmeter
+wget -O jmeter/apache-jmeter-3.0.tgz http://mirror.dkm.cz/apache//jmeter/binaries/apache-jmeter-3.0.tgz -q
+tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
+
+wget -O jmeter/apache-jmeter-3.0/lib/ext/cmdrunner-2.0.jar http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
+
+cp config/user.properties /jmeter/apache-jmeter-3.0/bin/
