eefd9d64ebffb0d1f58bd82341c22f2bf9045805
MokrejBandita <ondrej.mudra@tesena.com>
2016-08-31 16:01:53 +0200

    fixed path for copy user.properties

--- script/prepareJmeter.sh
+++ script/prepareJmeter.sh
@@ -6,4 +6,4 @@ tar zxf jmeter/apache-jmeter-3.0.tgz -C jmeter/
 
 wget -O jmeter/apache-jmeter-3.0/lib/ext/cmdrunner-2.0.jar http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
 
-cp config/user.properties /jmeter/apache-jmeter-3.0/bin/
+cp config/user.properties jmeter/apache-jmeter-3.0/bin/
